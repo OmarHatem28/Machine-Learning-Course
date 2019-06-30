21a0451de039c1a020475aa8dc6e6d3e0ce29f0c
Omar <omarh.ismail1@gmail.com>
2019-06-30 18:00:45 +0200

    Calculated house price

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -82,7 +82,7 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
+alpha = 0.1;
 num_iters = 400;
 
 % Init Theta and Run Gradient Descent 
@@ -105,7 +105,11 @@ fprintf('\n');
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
 price = 0; % You should change this
+house = [1650, 3];
+house = (house - mu) ./ sigma;
+house = [ones(1,1), house]
 
+price = house * theta;
 
 % ============================================================
 
